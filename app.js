7586dc47a21db4c0ffbd57db729aefaf1501f8fb
ChrisJeong <crowjdh@gmail.com>
2018-04-04 21:05:58 +0900

    Add log

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    console.log('Hi, there');
+    res.send('Hello World!');
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
